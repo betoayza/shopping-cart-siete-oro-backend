a171af6eab18428195286dad6d5413a9bef469c5
betodev <aaayza@gmail.com>
2023-11-09 19:20:15 -0300

    Some updates

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 
 const { Schema } = mongoose;
-
 const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@sietedeoro.pmni0.mongodb.net/sietedeoro?retryWrites=true&w=majority`;
-
 const conn = mongoose.createConnection(URI);
 
 conn.once("open", () => {
